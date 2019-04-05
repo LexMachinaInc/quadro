265c7e236c751e391ab1b944a72253e88fe94657
Sonny <sonny.sheth@gmail.com>
2019-04-05 10:15:10 -0300

    stop rendering zube labels

--- src/components/Label.js
+++ src/components/Label.js
@@ -3,6 +3,9 @@ import { lightOrDark } from "../helpers/utils";
 
 export default function Label({ labels }) {
   return labels.map(label => {
+    if (label.name.includes("zube")) {
+      return null;
+    }
     const background = lightOrDark(label.color);
     return (
       <span className={`github-label ${background}`} style={{
