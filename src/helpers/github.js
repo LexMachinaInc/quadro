74c2e36ced532019e31469f31318b5a2b39a7129
Sonny <sonny.sheth@gmail.com>
2019-04-25 18:31:44 -0300

    query for issue id

--- src/helpers/github.js
+++ src/helpers/github.js
@@ -109,6 +109,7 @@ export const GET_BUCKET = gql`
               nodes {
                 name
                 color
+                id
               }
             }
             title
@@ -132,6 +133,7 @@ export const GET_BUCKET = gql`
               nodes {
                 name
                 color
+                id
               }
             }
             number
