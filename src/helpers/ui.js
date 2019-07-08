57c34b27619985202486b4ef3311eb2fc04ac9be
Sonny <sonny.sheth@gmail.com>
2019-07-08 18:16:11 -0300

    on drop, fetch updated labels list

--- src/helpers/ui.js
+++ src/helpers/ui.js
@@ -1,4 +1,6 @@
 import { CONFIG } from "./github";
+import { getToken } from "./authorization";
+import axios from "axios";
 
 function getSearchParams() {
   return new URLSearchParams(window.location.search);
@@ -48,17 +50,26 @@ export function toggleDisplay(title) {
   bucket.classList.toggle("js-hide");
 }
 
-export function handleOnDrop(e, statusLabelId, allStatusLabels, updateIssue) {
+export async function getLabelsForIssue(url) {
+  const token = await getToken();
+  return axios
+    .get(url, { headers: { Authorization: `Bearer ${token}` } })
+    .then(data => data.data)
+}
+
+function extractLabels(labels, allStatusLabels) {
+  return labels
+    .filter((label) => !allStatusLabels.has(label.name))
+    .map((label) => label.name);
+}
+
+export async function handleOnDrop(e, statusLabelId, allStatusLabels, updateIssue) {
   const { originStatusLabelId, number, labels, url } = JSON.parse(e.dataTransfer.getData("text/plain"));
   if (statusLabelId === originStatusLabelId) {
     return;
   }
 
   const { owner } = CONFIG;
-  const originStatusName = originStatusLabelId ? allStatusLabels.find((label) => label.id === originStatusLabelId).name : null;
-  const updatedLabelNames = labels
-    .filter((label) => label.name !== originStatusName)
-    .map((label) => label.name);
 
   const getRepoName = (url) => {
     const parts = url.split("/")
@@ -67,11 +78,20 @@ export function handleOnDrop(e, statusLabelId, allStatusLabels, updateIssue) {
   };
 
   const repo = getRepoName(url);
+
+  const allStatusLabelNames = new Set(allStatusLabels.map((label) => label.name));
+  // `https://api.github.com/orgs/${owner}/issues?filter=assigned&sort=updated`
+  const fetchedIssues = await getLabelsForIssue(`https://api.github.com/repos/${owner}/${repo}/issues/${number}`);
+
+  //const issueToUpdate = fetchedIssues.find((issue) => issue.number === number && issue.repository.name);
+
+  const labelsToUpdate = fetchedIssues ? extractLabels(fetchedIssues.labels, allStatusLabelNames) : extractLabels(labels, allStatusLabelNames);
+
   // Moving card into Closed bucket
   if (!statusLabelId) {
-    updateIssue({ variables: { owner, repo, issue: number, labels: updatedLabelNames, state: "closed" }});
+    updateIssue({ variables: { owner, repo, issue: number, labels: labelsToUpdate, state: "closed" }});
   } else {
-    updatedLabelNames.push(allStatusLabels.find((label) => label.id === statusLabelId).name);
-    updateIssue({ variables: { owner, repo, issue: number, labels: updatedLabelNames, state: "open" }});
+    labelsToUpdate.push(allStatusLabels.find((label) => label.id === statusLabelId).name);
+    updateIssue({ variables: { owner, repo, issue: number, labels: labelsToUpdate, state: "open" }});
   }
 }
