0ea0b4a9651053c40f9befbb62ca15cf35180299
Gavin Carothers <gavin@carothers.name>
2022-07-18 17:02:39 -0700

    Split out dashboard interactive parts

--- src/components/DashBoard.jsx
+++ src/components/DashBoard.jsx
@@ -6,17 +6,56 @@ import { toggleSideMenu } from "../helpers/ui";
 import { useRepoInfo } from "../contexts/githubRepoInfo";
 import { useActiveBoard } from "../contexts/activeBoard";
 
-export default function DashBoard({ authenticated }) {
+function InteractiveElements() {
+  const { member, members, userAvatar } = useRepoInfo();
   const {
     activeBoardState: { activeBoardName: activeBoard },
     activeBoardMethods: { setActiveBoardName },
   } = useActiveBoard();
-  const { loading, member, members, userAvatar } = useRepoInfo();
   const onChangeHandler = (e) => {
     e.currentTarget.blur();
     const { value } = e.target;
     setActiveBoardName(value);
   };
+  return (
+    <>
+      <li className="member-dropdown">
+        <label className="member-select-label" htmlFor="member-select">
+          Board:
+        </label>
+        <select
+          onChange={onChangeHandler}
+          id="member-select"
+          className="select-css"
+          value={activeBoard}
+        >
+          {members.map((mem) => (
+            <option key={mem.login} value={mem.login}>
+              {mem.login}
+            </option>
+          ))}
+          <option disabled>_________</option>
+          {Object.entries(CONFIG.meetings).map((meeting) => (
+            <option key={meeting[0]} value={meeting[0]}>
+              {meeting[1]}
+            </option>
+          ))}
+        </select>
+      </li>
+      <li className="user-profile-container">
+        <button
+          type="button"
+          onClick={toggleSideMenu}
+          onKeyPress={toggleSideMenu}
+        >
+          <img className="user-avatar" src={userAvatar} alt={member} />
+        </button>
+      </li>
+    </>
+  );
+}
+export default function DashBoard({ authenticated }) {
+  const { loading } = useRepoInfo();
 
   return (
     <nav className="flexContainer blueBackground">
@@ -27,40 +66,7 @@ export default function DashBoard({ authenticated }) {
       </ul>
       <ul className="nav flexContainer flexEnd">
         {authenticated && !loading ? (
-          <>
-            <li className="member-dropdown">
-              <label className="member-select-label" htmlFor="member-select">
-                Board:
-              </label>
-              <select
-                onChange={onChangeHandler}
-                id="member-select"
-                className="select-css"
-                value={activeBoard}
-              >
-                {members.map((mem) => (
-                  <option key={mem.login} value={mem.login}>
-                    {mem.login}
-                  </option>
-                ))}
-                <option disabled>_________</option>
-                {Object.entries(CONFIG.meetings).map((meeting) => (
-                  <option key={meeting[0]} value={meeting[0]}>
-                    {meeting[1]}
-                  </option>
-                ))}
-              </select>
-            </li>
-            <li className="user-profile-container">
-              <button
-                type="button"
-                onClick={toggleSideMenu}
-                onKeyPress={toggleSideMenu}
-              >
-                <img className="user-avatar" src={userAvatar} alt={member} />
-              </button>
-            </li>
-          </>
+          <InteractiveElements></InteractiveElements>
         ) : null}
       </ul>
     </nav>
